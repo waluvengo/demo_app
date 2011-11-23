c3a6070b0ec5f426858b11fa4467a630513fe037
Virtual Machine Administrator <administrator@ubuntu-ror-dev-env.(none)>
2011-11-23 13:03:17 +0000

    Done with the demo app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '2a26025757ab791d262a3e88f415e84f2eab06585944219e90009b717ae4fbd780b2c496b5e1797e93abc4c9c0221cb9f58aa3bd7435c2d38ea2605031569e90'
